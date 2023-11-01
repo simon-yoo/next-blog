bec959c9c8f88ea84e6ceeac0f50e595ab8f0634
Simon Yoo <yoosghn0@gmail.com>
2023-11-01 18:42:03 +1300

    menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 import styles from './menu.module.css';
+import Link from 'next/link';
 const Menu = () => {
-  return <div className={styles.container}>Menu</div>;
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.subtitle}>{"What's hot"}</h2>
+      <h1 className={styles.title}>Most Popular</h1>
+      <div className={styles.items}>
+        <Link href='/' className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image src='/p1.jpeg' alt='' fill className={styles.image} />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.travel}`}>
+              Travel
+            </span>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>Simon Yoo</span>
+              <span className={styles.date}>01.11.2023</span>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
