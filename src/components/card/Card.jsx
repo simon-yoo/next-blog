e025ee3d5f01042c5215cc46484e4ea1461ac5fe
Simon Yoo <yoosghn0@gmail.com>
2023-11-01 17:15:58 +1300

    cards

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import styles from './card.module.css';
+import Link from 'next/link';
+
+const Card = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image src='/p1.jpeg' alt='' fill className={styles.image} />
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.details}>
+          <span className={styles.date}>11.02.2023 = </span>
+          <span className={styles.category}>CULTURE</span>
+        </div>
+        <Link href='/'>
+          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+        </Link>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia quo
+          adipisci doloremque quaerat deserunt ratione repudiandae expedita
+          quisquam enim illo incidunt velit dolores, a itaque? Odio ut numquam
+          ad facere?
+        </p>
+        <Link href='/' className={styles.link}>
+          Read More
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import styles from './cardList.module.css';
 import Pagination from '../pagination/Pagination';
 import Image from 'next/image';
+import Card from '../card/Card';
 const CardList = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Posts</h1>
       <div className={styles.posts}>
         <div className={styles.post}>
-          <div className={styles.imageContainer}>
-            <Image src='/p1.jpeg' alt='' fill />
-          </div>
-          <div className={styles.textContainer}></div>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
         </div>
       </div>
       <Pagination />
