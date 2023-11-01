074dccef899cfe5e74c04bd71cc85caec3e4d74e
Simon Yoo <yoosghn0@gmail.com>
2023-11-01 19:45:32 +1300

    menucategories and css

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import styles from './menu.module.css';
-import Link from 'next/link';
-import Image from 'next/image';
 import MenuPosts from '../menuPosts/MenuPosts';
+import MenuCategories from '../menuCategories/MenuCategories';
 
 const Menu = () => {
   return (
@@ -12,44 +11,7 @@ const Menu = () => {
       <MenuPosts withImage={false} />
       <h2 className={styles.subtitle}>Discover by topic</h2>
       <h1 className={styles.title}>Categories</h1>
-      <div className={styles.categoryList}>
-        <Link
-          href='/blog?cat=style'
-          className={`${styles.categoryItme} ${styles.style}`}
-        >
-          Style
-        </Link>
-        <Link
-          href='/blog?cat=style'
-          className={`${styles.categoryItme} ${styles.fashion}`}
-        >
-          Fashion
-        </Link>
-        <Link
-          href='/blog?cat=style'
-          className={`${styles.categoryItme} ${styles.food}`}
-        >
-          Food
-        </Link>
-        <Link
-          href='/blog?cat=style'
-          className={`${styles.categoryItme} ${styles.travel}`}
-        >
-          Travel
-        </Link>
-        <Link
-          href='/blog?cat=style'
-          className={`${styles.categoryItme} ${styles.culture}`}
-        >
-          Culture
-        </Link>
-        <Link
-          href='/blog?cat=style'
-          className={`${styles.categoryItme} ${styles.coding}`}
-        >
-          Coding
-        </Link>
-      </div>
+      <MenuCategories />
       <h2 className={styles.subtitle}>Chosen by the editor</h2>
       <h1 className={styles.title}>Editors Pick</h1>
       <MenuPosts withImage={true} />
