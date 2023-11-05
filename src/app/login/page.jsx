8936e3c658754d505b8075922f8defe6ba56aef5
Simon Yoo <yoosghn0@gmail.com>
2023-11-06 12:32:06 +1300

    created loginpage

---
+++ src/app/login/page.jsx
@@ -0,0 +1,14 @@
+import styles from './loginPage.module.css'
+const page = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.socialButton}>Sing in with Google</div>
+        <div className={styles.socialButton}>Sing in with Github</div>
+        <div className={styles.socialButton}>Sing in with Facebook</div>
+      </div>
+    </div>
+  )
+}
+
+export default page
