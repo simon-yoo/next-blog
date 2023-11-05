c390ac8ab9d04143b7537fb0a265109497d0d3ff
Simon Yoo <yoosghn0@gmail.com>
2023-11-06 12:53:22 +1300

    created buttons in writepage

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+'use client'
+
+import Image from 'next/image'
+import styles from './write.module.css'
+import { useState } from 'react'
 
 const WritePage = () => {
-  return <div>Write</div>
+  const [open, setIsOpen] = useState(false)
+  return (
+    <div className={styles.container}>
+      <input type='text' placeholder='Title' />
+      <div className={styles.eidtor}>
+        <button className={styles.button}>
+          <Image src='/plus.png' alt='' width={16} height={16} />
+        </button>
+        {open && (
+          <div className={styles.add}>
+            <button className={styles.addButton}>
+              <Image src='/image.png' alt='' width={16} height={16} />
+            </button>
+            <button className={styles.addButton}>
+              <Image src='/external.png' alt='' width={16} height={16} />
+            </button>
+            <button className={styles.addButton}>
+              <Image src='/video.png' alt='' width={16} height={16} />
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default WritePage
