62a9451fa15b3e72f1d1d4fa801837c99a5d1cfb
Simon Yoo <yoosghn0@gmail.com>
2023-11-06 19:25:26 +1300

    fix error

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient()
 } else {
   if (!global.prisma) {
-    global.prisma = new Prismaclient()
+    global.prisma = new PrismaClient()
   }
   prisma = global.prisma
 }
