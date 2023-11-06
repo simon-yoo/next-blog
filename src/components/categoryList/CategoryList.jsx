4d7359db43cc4b20df7f6b17492ed1781c5f0658
Simon Yoo <yoosghn0@gmail.com>
2023-11-06 21:26:18 +1300

    fetch data with category lists

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,94 +1,48 @@
-import React from 'react';
-import styles from './category.module.css';
-import Image from 'next/image';
-import Link from 'next/link';
-const Category = () => {
+import React from 'react'
+import styles from './category.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const getData = async () => {
+  const res = await fetch('http://localhost:3000//api/categories', {
+    cache: 'no-store',
+  })
+
+  if (!res.ok) {
+    throw new Error('Failed')
+  }
+
+  return res.json()
+}
+
+const CategoryList = async () => {
+  const data = await getData()
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Popular Categories</h1>
       <div className={styles.categories}>
-        <Link
-          href='/blog?cat=style'
-          className={`${styles.category} ${styles.style}`}
-        >
-          <Image
-            src='/style.png'
-            alt=''
-            width={32}
-            height={32}
-            className={styles.image}
-          />
-          Style
-        </Link>
-        <Link
-          href='/blog?cat=style'
-          className={`${styles.category} ${styles.fashion}`}
-        >
-          <Image
-            src='/fashion.png'
-            alt=''
-            width={32}
-            height={32}
-            className={styles.image}
-          />
-          Fashion
-        </Link>
-        <Link
-          href='/blog?cat=style'
-          className={`${styles.category} ${styles.food}`}
-        >
-          <Image
-            src='/food.png'
-            alt=''
-            width={32}
-            height={32}
-            className={styles.image}
-          />
-          food
-        </Link>
-
-        <Link
-          href='/blog?cat=style'
-          className={`${styles.category} ${styles.travel}`}
-        >
-          <Image
-            src='/travel.png'
-            alt=''
-            width={32}
-            height={32}
-            className={styles.image}
-          />
-          Travel
-        </Link>
-        <Link
-          href='/blog?cat=style'
-          className={`${styles.category} ${styles.culture}`}
-        >
-          <Image
-            src='/culture.png'
-            alt=''
-            width={32}
-            height={32}
-            className={styles.image}
-          />
-          Culture
-        </Link>
-        <Link
-          href='/blog?cat=style'
-          className={`${styles.category} ${styles.coding}`}
-        >
-          <Image
-            src='/coding.png'
-            alt=''
-            width={32}
-            height={32}
-            className={styles.image}
-          />
-          Coding
-        </Link>
+        {data?.map((item) => (
+          <Link
+            href='/blog?cat=style'
+            key={item._id}
+            className={`${styles.category} ${styles[item.slug]}`}
+          >
+            {item.img && (
+              <Image
+                src={item.img}
+                alt=''
+                width={32}
+                height={32}
+                className={styles.image}
+              />
+            )}
+            {item.title}
+          </Link>
+        ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Category;
+export default CategoryList
