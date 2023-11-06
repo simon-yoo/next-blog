827a0f2796cb6673b991ab161f68718e95a59392
Simon Yoo <yoosghn0@gmail.com>
2023-11-06 13:06:56 +1300

    added classname for input and ReactQuill

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Image from 'next/image'
-import styles from './write.module.css'
+import styles from './writePage.module.css'
 import { useState } from 'react'
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.bubble.css'
@@ -11,9 +11,9 @@ const WritePage = () => {
   const [value, setVaule] = useState('')
   return (
     <div className={styles.container}>
-      <input type='text' placeholder='Title' />
-      <div className={styles.eidtor}>
-        <button className={styles.button}>
+      <input type='text' placeholder='Title' className={styles.input} />
+      <div className={styles.editor}>
+        <button className={styles.button} onClick={() => setOpen(!open)}>
           <Image src='/plus.png' alt='' width={16} height={16} />
         </button>
         {open && (
@@ -30,6 +30,7 @@ const WritePage = () => {
           </div>
         )}
         <ReactQuill
+          className={styles.textArea}
           theme='bubble'
           value={value}
           onChange={setVaule}
