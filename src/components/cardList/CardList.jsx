a01c337c668e9c065b7072b13524a2af76de4492
Simon Yoo <yoosghn0@gmail.com>
2023-11-08 10:47:07 +1300

    fetch api

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
-import styles from './cardList.module.css';
-import Pagination from '../pagination/Pagination';
-import Image from 'next/image';
-import Card from '../card/Card';
-const CardList = () => {
+import React from 'react'
+import styles from './cardList.module.css'
+import Pagination from '../pagination/Pagination'
+import Image from 'next/image'
+import Card from '../card/Card'
+
+const getData = async () => {
+  const res = await fetch('http://localhost:3000//api/posts', {
+    cache: 'no-store',
+  })
+
+  if (!res.ok) {
+    throw new Error('Failed')
+  }
+
+  return res.json()
+}
+
+const CardList = async () => {
+  const data = await getData()
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Posts</h1>
@@ -17,7 +31,7 @@ const CardList = () => {
       </div>
       <Pagination />
     </div>
-  );
-};
+  )
+}
 
-export default CardList;
+export default CardList
