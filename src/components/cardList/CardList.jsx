9d0a39878e2a84c80d9c9dcccf37fe47cd88b1d4
Simon Yoo <yoosghn0@gmail.com>
2023-11-08 16:49:15 +1300

    fetch posts and count

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -17,18 +17,24 @@ const getData = async (page) => {
 }
 
 const CardList = async ({ page }) => {
-  const data = await getData(page)
+  const { posts, count } = await getData(page)
+
+  const POST_PER_PAGE = 2
+
+  const hasPrev = POST_PER_PAGE * (page - 1) > 0
+  const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Posts</h1>
       <div className={styles.posts}>
         <div className={styles.post}>
-          {data?.map((item) => (
+          {posts?.map((item) => (
             <Card item={item} key={item._id} />
           ))}
         </div>
       </div>
-      <Pagination />
+      <Pagination page={page} hasNext={hasNext} hasPrev={hasPrev} />
     </div>
   )
 }
