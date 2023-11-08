363388e1300366fc226fed99faaea252fb9dfbb0
Simon Yoo <yoosghn0@gmail.com>
2023-11-08 16:40:18 +1300

    add userouter for next and prev page

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import styles from './pagination.module.css';
-const Pagination = () => {
+'use client'
+
+import React from 'react'
+import styles from './pagination.module.css'
+import { useRouter } from 'next/navigation'
+
+const Pagination = ({ page }) => {
+  const router = useRouter()
+
   return (
     <div className={styles.container}>
-      <button className={styles.button}>Previous</button>
-      <button className={styles.button}>Next</button>
+      <button
+        className={styles.button}
+        onClick={() => router.push`?page=${page - 1}`}
+      >
+        Previous
+      </button>
+      <button
+        className={styles.button}
+        onClick={() => router.push(`?page=${page + 1}`)}
+      >
+        Next
+      </button>
     </div>
-  );
-};
+  )
+}
 
-export default Pagination;
+export default Pagination
