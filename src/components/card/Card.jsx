c61562e8a8ec1254092a82aab4c9b92ef100fe8a
Simon Yoo <yoosghn0@gmail.com>
2023-11-09 19:31:45 +1300

    fixing error

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,13 +5,15 @@ import Link from 'next/link'
 const Card = ({ key, item }) => {
   return (
     <div className={styles.container} key={key}>
+      {/* {item.Image && ( */}
       <div className={styles.imageContainer}>
         <Image src='/p1.jpeg' alt='' fill className={styles.image} />
       </div>
+      {/* )} */}
       <div className={styles.textContainer}>
         <div className={styles.details}>
           <span className={styles.date}>11.02.2023 = </span>
-          <span className={styles.category}>CULTURE</span>
+          <span className={styles.category}>{item.catSlug}</span>
         </div>
         <Link href='/'>
           <h1>{item.title}</h1>
