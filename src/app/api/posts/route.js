243531f52b2b85bada062fb9d399c511b023b748
Simon Yoo <yoosghn0@gmail.com>
2023-11-09 23:23:54 +1300

    filter posts throug category

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -5,6 +5,7 @@ export const GET = async (req) => {
   const { searchParams } = new URL(req.url)
 
   const page = searchParams.get('page')
+  const cat = searchParams.get('cat')
   // const cat = serachParams.get('cat')
 
   const POST_PER_PAGE = 2
@@ -12,12 +13,15 @@ export const GET = async (req) => {
   const query = {
     take: POST_PER_PAGE,
     skip: POST_PER_PAGE * (page - 1),
+    where: {
+      ...(cat && { catSlug: cat }),
+    },
   }
 
   try {
     const [posts, count] = await prisma.$transaction([
       prisma.post.findMany(query),
-      prisma.post.count(),
+      prisma.post.count({ where: query.where }),
     ])
 
     return new NextResponse(JSON.stringify({ posts, count }, { status: 200 }))
