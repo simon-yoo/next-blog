aad5ea48ef2cda37e282664ad18c3522cc8f5ebe
Simon Yoo <yoosghn0@gmail.com>
2023-11-09 23:59:44 +1300

    fetching slug in singlepage

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -1,19 +1,35 @@
-import Image from 'next/image';
-import styles from './singlePage.module.css';
-import Menu from '@/components/menu/Menu';
-import Comment from '@/components/comments/Comment';
-const SinglePage = () => {
+import Image from 'next/image'
+import styles from './singlePage.module.css'
+import Menu from '@/components/menu/Menu'
+import Comment from '@/components/comments/Comment'
+
+const getData = async (slug) => {
+  const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+    cache: 'no-store',
+  })
+
+  if (!res.ok) {
+    throw new Error('Failed')
+  }
+
+  return res.json()
+}
+
+const SinglePage = async ({ params }) => {
+  const { slug } = params
+
+  const data = await getData(slug)
   return (
     <div className={styles.container}>
       <div className={styles.infoContainer}>
         <div className={styles.textContainer}>
-          <h1 className={styles.title}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          </h1>
+          <h1 className={styles.title}>{data?.title}</h1>
           <div className={styles.user}>
-            <div className={styles.userImageContainer}>
-              <Image src='/p1.jpeg' alt='' fill className={styles.avatar} />
-            </div>
+            {data?.img && (
+              <div className={styles.userImageContainer}>
+                <Image src={data.img} alt='' fill className={styles.avatar} />
+              </div>
+            )}
             <div className={styles.userTextContainer}>
               <span className={styles.username}>Simon Yoo</span>
               <span className={styles.date}>01.11.2023</span>
@@ -26,27 +42,10 @@ const SinglePage = () => {
       </div>
       <div className={styles.content}>
         <div className={styles.post}>
-          <div className={styles.description}>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-              nulla hic maiores vel neque commodi optio autem. Eaque optio
-              placeat doloremque expedita in illum. Quisquam consequuntur ipsa
-              perferendis amet adipisci.
-            </p>
-            <h2>Lorem ipsum dolor sit amet.</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-              nulla hic maiores vel neque commodi optio autem. Eaque optio
-              placeat doloremque expedita in illum. Quisquam consequuntur ipsa
-              perferendis amet adipisci.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-              nulla hic maiores vel neque commodi optio autem. Eaque optio
-              placeat doloremque expedita in illum. Quisquam consequuntur ipsa
-              perferendis amet adipisci.
-            </p>
-          </div>
+          <div
+            className={styles.description}
+            dangerouslySetInnerHTML={{ __html: data?.desc }}
+          />{' '}
           <div className={styles.comment}>
             <Comment />
           </div>
@@ -54,7 +53,7 @@ const SinglePage = () => {
         <Menu />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default SinglePage;
+export default SinglePage
