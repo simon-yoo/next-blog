bde70a8affd17e45462e2368feb3ca052137ebda
Simon Yoo <yoosghn0@gmail.com>
2023-11-09 23:04:31 +1300

    fix error

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,11 +5,11 @@ import Link from 'next/link'
 const Card = ({ key, item }) => {
   return (
     <div className={styles.container} key={key}>
-      {/* {item.Image && ( */}
-      <div className={styles.imageContainer}>
-        <Image src='/p1.jpeg' alt='' fill className={styles.image} />
-      </div>
-      {/* )} */}
+      {item.img && (
+        <div className={styles.imageContainer}>
+          <Image src={item.img} alt='' fill className={styles.image} />
+        </div>
+      )}
       <div className={styles.textContainer}>
         <div className={styles.details}>
           <span className={styles.date}>11.02.2023 = </span>
@@ -18,12 +18,7 @@ const Card = ({ key, item }) => {
         <Link href='/'>
           <h1>{item.title}</h1>
         </Link>
-        <p className={styles.desc}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia quo
-          adipisci doloremque quaerat deserunt ratione repudiandae expedita
-          quisquam enim illo incidunt velit dolores, a itaque? Odio ut numquam
-          ad facere?
-        </p>
+        <p className={styles.desc}>{item.desc}</p>
         <Link href='/' className={styles.link}>
           Read More
         </Link>
